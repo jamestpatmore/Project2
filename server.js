d7fd336f335436664cb6b33f686b5328fdf0bda4
jschuyl <schuylertjed@gmail.com>
2022-11-04 14:25:24 -0700

    something is broken, I'll need a second pair of eyes to help

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./routes');
+//const helpers = require('./utils/helpers');
 
-const PORT = 3001;
+const sequelize = require('./config/connection');
+//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send(
-    `<p>DevTek<p>`
-  );
+//const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  /*store: new SequelizeStore({
+    db: sequelize
+  })*/
+};
+
+app.use(session(sess));
+
+//app.engine('handlebars', hbs.engine);
+//app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
-
-app.get('/api', (req, res) => {
-  res.json({
-    term: 'api',
-    description:
-      'theres nothing here yet go away',
-  });
-});
-
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
-
