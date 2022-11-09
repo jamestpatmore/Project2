62a5a2e670538d9c12bcbdfa74b95d16bf298a80
james.patmore <110008934+jamestpatmore@users.noreply.github.com>
2022-11-09 15:26:10 -0700

    setting a foundation

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,7 @@
+/*
+Tools??
+
+
 function burgerToggle() {
 var burger = document.querySelector('.navbar-burger');
 var menu = document.querySelector('.navbar-menu');
@@ -18,3 +22,5 @@ spinner = document.querySelector("#spinner");
 if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
 spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
 }
+
+*/
