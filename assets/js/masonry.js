c62cc2db0ba646130160987894497ac15048c66d
jschuyl <schuylertjed@gmail.com>
2022-11-08 17:46:09 -0700

    started messing with masonry, keep running into 'Failed to resolve module specifier masonry-layout' error and I am find that very annoying

--- assets/js/masonry.js
+++
@@ -1,5 +0,0 @@
-import Masonry from 'masonry-layout';
-
-window.onload = () => {
-    const grid = document.querySelector
-}
---
+++ assets/js/masonry.mjs
@@ -0,0 +1,9 @@
+import Masonry from 'masonry-layout';
+
+window.onload = () => {
+    const grid = document.querySelector('.grid');
+
+    const masonry = new Masonry(grid, {
+        itemSelector: '.grid-item'
+    });
+}
