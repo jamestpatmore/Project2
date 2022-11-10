c6fe9d51e4ac7f5d152b78023858f03afb6bb158
jschuyl <schuylertjed@gmail.com>
2022-11-09 20:59:19 -0700

    got masonry working, just gotta get more stylizing

--- public/js/masonry.js
+++ public/js/masonry.js
@@ -1,7 +1,8 @@
-import Masonry from 'masonry-layout';
-
 window.onload = () => {
     const grid = document.querySelector('.grid');
 
-    const msnry = new Masonry(grid);
-};
+    const msnry = new Masonry(grid, {
+        itemSelector: '.grid-item',
+        columnWidth: 80
+    });
+}
