3895d32a6a1ddecb37596756a2d1b73223908ba3
jschuyl <schuylertjed@gmail.com>
2022-11-09 20:17:15 -0700

    trying to get masonry to work with handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const path = require('path');
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 // Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
+/*router.get('/', withAuth, async (req, res) => {
   try {
     const userData = await User.findOne({
       where: {
@@ -22,7 +23,7 @@ router.get('/', withAuth, async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-});
+});*/
 
 router.get('/login', (req, res) => {
   // If a session exists, redirect the request to the homepage
@@ -32,6 +33,10 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/blog', async (req, res) => {
+  res.render('blog');
 });
 
 module.exports = router;
--- public/js/masonry.js
+++ public/js/masonry.js
@@ -3,7 +3,5 @@ import Masonry from 'masonry-layout';
 window.onload = () => {
     const grid = document.querySelector('.grid');
 
-    const msnry = new Masonry(grid, {
-        itemSelector: '.grid-item'
-    });
-}
+    const msnry = new Masonry(grid);
+};
